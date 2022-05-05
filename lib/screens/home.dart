f31f0a431ecc4781d3cc0e2856b6cbf59b524f37
augustineonu <onuaugustine07@gmail.com>
2022-05-05 10:30:21 +0100

    ft(company): -merchant home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:trakk/screens/auth/login.dart';
 import 'package:trakk/screens/auth/signup.dart';
 import 'package:trakk/screens/dispatch/item_details.dart';
+import 'package:trakk/screens/merchant/signup_merchant.dart';
 import 'package:trakk/utils/colors.dart';
 import 'package:trakk/widgets/button.dart';
 
@@ -78,7 +79,9 @@ class _HomeState extends State<Home> {
             const SizedBox(height: 15.0),
             Button(
               text: 'Become a Merchant',
-              onPress: () {},
+              onPress: () {
+                Navigator.of(context).pushNamed(SignupMerchant.id);
+              },
               color: whiteColor,
               isLoading: false,
               textColor: appPrimaryColor,
---
+++ lib/widgets/merchant_container.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:trakk/utils/colors.dart';
+
+class MerchantContainer extends StatelessWidget {
+  final String title;
+  final Color color;
+  final String icon;
+  final String ?rides;
+
+  const MerchantContainer({
+    Key? key,
+    required this.color,
+    required this.icon,
+    required this.title, 
+    this.rides,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 7, right: 7),
+        height: 160,
+        width: 160,
+        decoration: BoxDecoration(
+          boxShadow:  const [
+              BoxShadow(
+                color: Color.fromARGB(255, 219, 219, 219),
+                spreadRadius: 2.0,
+                blurRadius: 5.0,
+                offset: Offset(5, 2), // changes position of shadow
+              ),
+            ],
+          
+            border: Border.all(color: boarderline ),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20)),
+        child: Stack(
+          children: [
+            Center(
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+              title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              textAlign: TextAlign.center,
+            ),
+            Text(rides?? '', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600), )
+                  ],
+                )),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SizedBox(
+                child: Container(
+                  height: 48,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: color,
+                      borderRadius: const BorderRadius.only(
+                          bottomRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(25),
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(5),),
+                          ),
+                  child: Center(child: SvgPicture.asset(icon)),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
