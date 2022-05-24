833051aa25a11b1e5867ca1d152cb13ec5ddc81d
augustineonu <onuaugustine07@gmail.com>
2022-05-24 16:26:57 +0100

    ft-merchant updating screens

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,10 @@ void main() async {
   // To load the .env file contents into dotenv.
   // NOTE: fileName defaults to .env and can be omitted in this case.
   // Ensure that the filename corresponds to the path in step 1 and 2.
+<<<<<<< HEAD
   await dotenv.load(fileName: ".env");
+=======
+>>>>>>> ab88d43 (ft-merchant changes)
   //await dotenv.load(fileName: ".env");
     
   runApp(const MyApp());
@@ -116,6 +119,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
+<<<<<<< HEAD
+=======
+    
+    //print(dotenv.env["PUSHER_TOKEN"]);
+
+    
+>>>>>>> ab88d43 (ft-merchant changes)
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarColor: appPrimaryColor));
     pusher.onConnectionStateChange((state) {
@@ -178,7 +188,12 @@ class _MyAppState extends State<MyApp> {
             CartScreen.id: (context) => const CartScreen(),
             PolylineScreen.id: (context) => const PolylineScreen(),
             UserOrderScreen.id: (context) => const UserOrderScreen(),
+<<<<<<< HEAD
             ProfileMenu.id:(context) => const ProfileMenu(),
+=======
+
+            UserMenu.id:(context) => const UserMenu(),
+>>>>>>> ab88d43 (ft-merchant changes)
             EditProfile.id:(context) => const EditProfile(),
             Help.id:(context) => const Help(),
             FundWalletScreen.id: (context) => const FundWalletScreen(),
@@ -193,8 +208,15 @@ class _MyAppState extends State<MyApp> {
             AllVehicleContainer.id:(context) => const AllVehicleContainer(),
             EditRiderProfile.id:(context) => const EditRiderProfile(),
             ListOfRiders.id:(context) => const ListOfRiders(),
+<<<<<<< HEAD
             RejectedRides.id:(context) => const RejectedRides(),
             FulfilledDispatch.id:(context) => const FulfilledDispatch(),
+=======
+
+            RejectedRides.id:(context) => const RejectedRides(),
+            FulfilledDispatch.id:(context) => const FulfilledDispatch(),
+
+>>>>>>> ab88d43 (ft-merchant changes)
             // MyDatePicker.id: (context) => MyDatePicker(),
             // Country.id: (context) => const Country(),
             ProfileMenu.id: (context) => const ProfileMenu(),
@@ -215,6 +237,10 @@ class _MyAppState extends State<MyApp> {
             AllVehicleContainer.id: (context) => const AllVehicleContainer(),
             EditRiderProfile.id: (context) => const EditRiderProfile(),
             ListOfRiders.id: (context) => const ListOfRiders(),
+<<<<<<< HEAD
+=======
+
+>>>>>>> ab88d43 (ft-merchant changes)
             RejectedRides.id:(context) => const RejectedRides(),
             FulfilledDispatch.id:(context) => const FulfilledDispatch(),
 
