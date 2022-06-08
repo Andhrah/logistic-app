9f82978b2607f39e980cc478c358daf6f3f7378e
augustineonu <onuaugustine07@gmail.com>
2022-06-08 11:23:14 +0100

    ft-wallet eidt rider profile

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ void main() async {
   // To load the .env file contents into dotenv.
   // NOTE: fileName defaults to .env and can be omitted in this case.
   // Ensure that the filename corresponds to the path in step 1 and 2.
-  await dotenv.load(fileName: ".env");
+ // await dotenv.load(fileName: ".env");
   //await dotenv.load(fileName: ".env");
     
   runApp(const MyApp());
@@ -103,7 +103,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  static final String?  _pusher = dotenv.env["PUSHER_TOKEN"];
+  //static final String?  _pusher = dotenv.env["PUSHER_TOKEN"];
 
   PusherOptions options = PusherOptions(
     host: "https://trakk-server.herokuapp.com",
